d11170e1c2292182bbecd59ba5633763b579a6e6
GoRhanHee <gorhanhee01@gmail.com>
2026-01-15 23:19:08 +0900

    build: import kernelsu option building system

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 # Setting
 export ANDROID_BUILD_TOP=$(pwd)
 export DEVICE=$1
+export KSU=$2
 
 # OEM Setting
 export ARCH=arm64
@@ -16,10 +17,17 @@ MAKE_ARGS="
 ARCH=arm64 \
 O=out
 "
+echo -e "\nCONFIG_MACH_EXYNOS9810_${DEVICE^^}_KOR=y" >> "arch/arm64/configs/gorhanhee.config"
 
 DEFCONFIG="exynos9810-${DEVICE}kor_defconfig"
 
-echo -e "\nCONFIG_MACH_EXYNOS9810_${DEVICE^^}_KOR=y" >> "arch/arm64/configs/gorhanhee.config"
+if [ "${KSU}" == "y" ]; then
+    CONFIGS="${DEFCONFIG} gorhanhee.config kernelsu.config"
+elif [ "${KSU}" == "n" ]; then
+    CONFIGS="${DEFCONFIG} gorhanhee.config"
+else
+    echo "Check KSU Oprion ex)./build.sh crownlte y"    
+fi
 
-make ${MAKE_ARGS} ${DEFCONFIG} gorhanhee.config|| exit 1
+make ${MAKE_ARGS} ${CONFIGS}|| exit 1
 make ${MAKE_ARGS} || exit 1
